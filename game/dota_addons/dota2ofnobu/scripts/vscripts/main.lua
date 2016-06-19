b7b5df3ee105e3e96b60e1f6fab830ddb680f96d
damody <t1238142000@gmail.com>
2016-06-19 15:57:58 +0800

    now can check people can play from my server http://218.161.33.54/dota2

--- game/dota_addons/dota2ofnobu/scripts/vscripts/main.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/main.lua
@@ -13,13 +13,66 @@ print ('[Nobu] main lua script Starting..' )
 
 --endglobal
 
+
+
 if NobuGameMode == nil then
 	NobuGameMode = class({})
 end
 
+
+
+function SendHTTPRequest(method, values, callback)
+	local req = CreateHTTPRequest( method, "http://218.161.33.54/dota2" )
+	for key, value in pairs(values) do
+		req:SetHTTPRequestGetOrPostParameter(key, value)
+	end
+	req:Send(function(result)
+		callback(result.Body)
+	end)
+end
+
+function NobuGameMode:OnPlayerConnectFull(keys)
+    local player = PlayerInstanceFromIndex(keys.index + 1)
+	local pID = keys.index
+	local steamID = PlayerResource:GetSteamAccountID(pID)
+    print("steamID "..steamID)
+	
+	SendHTTPRequest("POST",
+		{
+			id = tostring(steamID),
+		}, 
+		function(result)
+			print(result)
+			if (result == "error") then
+				player:Destroy()
+			end
+			-- Decode response into a lua table
+			local resultTable = {}
+			if not pcall(function()
+				resultTable = JSON:decode(result)
+			end) then
+				Warning("[dota2.tools.Storage] Can't decode result: " .. result)
+			end
+
+			-- If we get an error response, successBool should be false
+			if resultTable ~= nil and resultTable["errors"] ~= nil then
+				callback(resultTable["errors"], false)
+				return
+			end
+
+			-- If we get a success response, successBool should be true
+			if resultTable ~= nil and resultTable["data"] ~= nil  then
+				callback(resultTable["data"], true)
+				return
+			end
+		end
+	)
+
+end
+
 function NobuGameMode:InitGameMode()
 	print( "[Nobu] NobuGameMode:InitGameMode is loaded." )
-
+	ListenToGameEvent('player_connect_full', Dynamic_Wrap(NobuGameMode, 'OnPlayerConnectFull'), self)
 	-- 產生隨機數種子，主要是為了程序中的隨機數考慮
 	local timeTxt = string.gsub(string.gsub(GetSystemTime(), ':', ''), '0','') 
 	math.randomseed(tonumber(timeTxt))--GetSystemTime	string GetSystemTime()	獲取真實世界的時間
