1d55b99ee24a78cfa72f9fff50c0607187eccf2c
damody <t1238142000@gmail.com>
2016-09-06 00:17:06 +0800

    修正擴散 120度 到 360度

--- game/dota_addons/dota2ofnobu/scripts/vscripts/heroes/C_Neutral/C10.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/heroes/C_Neutral/C10.lua
@@ -72,3 +72,26 @@ function C10W( keys )
 	}
 	caster.powershot_projectileID = ProjectileManager:CreateLinearProjectile( projectileTable )
 end
+
+
+function C10R(keys)
+	--【Basic】
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+	local level = ability:GetLevel() - 1
+	local dmg = keys.dmg
+	local per_atk = 0
+	local targetArmor = target:GetPhysicalArmorValue()
+	local damageReduction = ((0.06 * targetArmor) / (1 + 0.06 * targetArmor))
+	local dmg = dmg / (1 - damageReduction)
+	local group = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(),
+		nil, 385 , DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, 0, false)
+
+	dmg = dmg * ability:GetLevelSpecialValueFor("CleavePercent",level)
+	for _, it in pairs(group) do
+		if it ~= target then
+			AMHC:Damage( caster,it,dmg,AMHC:DamageType( "DAMAGE_TYPE_PHYSICAL" ) )
+		end
+	end
+end
