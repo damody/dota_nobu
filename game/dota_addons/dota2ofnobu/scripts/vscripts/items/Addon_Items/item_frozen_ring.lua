fba3ab74fcbe96f777609bb3b42aee747f280617
fiaincub <fiaincub@gmail.com>
2016-09-21 00:17:10 +0800

    新增凍雲、雪走、禦神法輪

--- game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_frozen_ring.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_frozen_ring.lua
@@ -7,18 +7,18 @@ function Shock( keys )
 	local direUnits = FindUnitsInRadius(caster:GetTeamNumber(),
 						caster:GetAbsOrigin(),
 						nil,
-						350,
+						275,
 						DOTA_UNIT_TARGET_TEAM_ENEMY,
-						DOTA_UNIT_TARGET_ALL,
+						DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
 						DOTA_UNIT_TARGET_FLAG_NONE,
 						FIND_ANY_ORDER,
 						false)
 	for _,it in pairs(direUnits) do
-		ability:ApplyDataDrivenModifier(caster, it,"modifier_frozen_ring",{duration=5})
-		AMHC:Damage(caster,it,350,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
+		ability:ApplyDataDrivenModifier(caster, it,"modifier_frozen_ring",{duration=1})
+		AMHC:Damage(caster,it,160,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
 	end
 	local particle = ParticleManager:CreateParticle("particles/a34e2/a34e2.vpcf", PATTACH_ABSORIGIN, caster)
-	Timers:CreateTimer(5, function ()
+	Timers:CreateTimer(1, function ()
 		ParticleManager:DestroyParticle(particle, true)
 		end)
 	--跟電卷共用CD
@@ -28,6 +28,94 @@ function Shock( keys )
 			local itemName = item:GetName()
 			if (itemName == "item_lightning_scroll") then
 				item:StartCooldown(17)
+			end
+		end
+	end
+end
+
+--雪走
+
+function Shock2( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
+	local direUnits = FindUnitsInRadius(caster:GetTeamNumber(),
+						target:GetAbsOrigin(),
+						nil,
+						200,
+						DOTA_UNIT_TARGET_TEAM_ENEMY,
+						DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+						DOTA_UNIT_TARGET_FLAG_NONE,
+						FIND_ANY_ORDER,
+						false)
+	AMHC:Damage(caster,target,300,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
+	for _,it in pairs(direUnits) do
+		ability:ApplyDataDrivenModifier(caster, it,"modifier_frozen_ring",{duration=7})
+		if it ~= target then
+		AMHC:Damage(caster,it,200,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
+		end
+	end
+	local particle = ParticleManager:CreateParticle("particles/a34e2/a34e2.vpcf", PATTACH_ABSORIGIN, target)
+	Timers:CreateTimer(7, function ()
+		ParticleManager:DestroyParticle(particle, true)
+		end)
+end
+
+--凍雲
+
+function OnEquip( keys )
+	local caster = keys.caster
+	if (caster.nobuorb1 == nil) then
+		caster.nobuorb1 = "item_kokumo"
+	end
+end
+
+function OnUnequip( keys )
+	local caster = keys.caster
+	if (caster.nobuorb1 == "item_kokumo") then
+		caster.nobuorb1 = nil
+	end
+end
+
+function Shock3( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
+	if (caster.nobuorb1 == "item_kokumo") and not target:IsBuilding() then
+		local ran =  RandomInt(0, 100)
+		if (caster.kokumo == nil) then
+			caster.kokumo = 0
+		end
+		if (ran > 20) then
+			caster.kokumo = caster.kokumo + 1
+		end
+		if (caster.kokumo > (100/18) or ran <= 18) then
+			caster.kokumo = 0
+		local direUnits = FindUnitsInRadius(caster:GetTeamNumber(),
+							target:GetAbsOrigin(),
+							nil,
+							325,
+							DOTA_UNIT_TARGET_TEAM_ENEMY,
+							DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+							DOTA_UNIT_TARGET_FLAG_NONE,
+							FIND_ANY_ORDER,
+							false)
+			if (not keys.target:IsMagicImmune() and keys.target.kokumo == nil) then
+					keys.target.kokumo = 1
+			Timers:CreateTimer(0.1, function() 
+							keys.target.kokumo = nil
+						end)
+			AMHC:Damage(caster,target,480,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
+			for _,it in pairs(direUnits) do
+				ability:ApplyDataDrivenModifier(caster, it,"modifier_frozen_ring",{duration=4.5})
+				if it ~= target then
+				AMHC:Damage(caster,it,280,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
+				end
+			end
+			local particle = ParticleManager:CreateParticle("particles/a34e2/a34e2.vpcf", PATTACH_ABSORIGIN, target)
+			Timers:CreateTimer(4.5, function ()
+				ParticleManager:DestroyParticle(particle, true)
+				end)
 			end
 		end
 	end
---
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_mana_thief.lua
@@ -0,0 +1,239 @@
+function OnEquip( keys )
+	local caster = keys.caster
+	if (caster.nobuorb1 == nil) then
+		caster.nobuorb1 = "item_mana_thief"
+	end
+end
+
+function OnUnequip( keys )
+	local caster = keys.caster
+	if (caster.nobuorb1 == "item_mana_thief") then
+		caster.nobuorb1 = nil
+	end
+	for itemSlot=0,5 do
+		local item = caster:GetItemInSlot(itemSlot)
+		if item ~= nil then
+			local itemName = item:GetName()
+			if (string.match(itemName,"item_mana_thief")) then
+				caster.nobuorb1 = "item_mana_thief"
+				break
+			end
+		end
+	end
+end
+
+function mana_burn_function( keys )
+	-- Variables
+	local caster = keys.caster
+	local target = keys.target
+	local current_mana = target:GetMana()
+	local burn_amount = 500
+	local number_particle_name = "particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn_msg.vpcf"
+	local burn_particle_name = "particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn.vpcf"
+	local damageType = keys.ability:GetAbilityDamageType()
+	
+	-- Calculation
+	local mana_to_burn = math.min( current_mana,  burn_amount)
+	local life_time = 2.0
+	local digits = string.len( math.floor( mana_to_burn ) ) + 1
+	
+	-- Fail check
+	if target:IsMagicImmune() then
+		mana_to_burn = 0
+	end
+	
+	-- Apply effect of ability
+	target:ReduceMana( mana_to_burn )
+	local damageTable = {
+		victim = target,
+		attacker = caster,
+		damage = mana_to_burn,
+		damage_type = damageType
+	}
+	ApplyDamage( damageTable )
+	
+	-- Show VFX
+	if mana_to_burn ~= 0 then
+		local numberIndex = ParticleManager:CreateParticle( number_particle_name, PATTACH_OVERHEAD_FOLLOW, target )
+		ParticleManager:SetParticleControl( numberIndex, 1, Vector( 1, mana_to_burn, 0 ) )
+	    ParticleManager:SetParticleControl( numberIndex, 2, Vector( life_time, digits, 0 ) )
+		local burnIndex = ParticleManager:CreateParticle( burn_particle_name, PATTACH_ABSORIGIN, target )
+		
+		-- Create timer to properly destroy particles
+		Timers:CreateTimer( life_time, function()
+				ParticleManager:DestroyParticle( numberIndex, false )
+				ParticleManager:DestroyParticle( burnIndex, false)
+				return nil
+			end
+		)
+	end
+	for itemSlot=0,5 do
+		local item = caster:GetItemInSlot(itemSlot)
+		if item ~= nil then
+			local itemName = item:GetName()
+			if (itemName == "item_mana_thief_1") then
+				item:StartCooldown(17)
+			end
+		end
+	end
+end
+
+function mana_burn_function2( keys )
+	-- Variables
+	local caster = keys.caster
+	local target = keys.target
+	local current_mana = target:GetMana()
+	local burn_amount = 600
+	local number_particle_name = "particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn_msg.vpcf"
+	local burn_particle_name = "particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn.vpcf"
+	local damageType = keys.ability:GetAbilityDamageType()
+	
+	-- Calculation
+	local mana_to_burn = math.min( current_mana,  burn_amount)
+	local life_time = 2.0
+	local digits = string.len( math.floor( mana_to_burn ) ) + 1
+	
+	-- Fail check
+	if target:IsMagicImmune() then
+		mana_to_burn = 0
+	end
+	
+	-- Apply effect of ability
+	target:ReduceMana( mana_to_burn )
+	local damageTable = {
+		victim = target,
+		attacker = caster,
+		damage = mana_to_burn,
+		damage_type = damageType
+	}
+	ApplyDamage( damageTable )
+	
+	-- Show VFX
+	if mana_to_burn ~= 0 then
+		local numberIndex = ParticleManager:CreateParticle( number_particle_name, PATTACH_OVERHEAD_FOLLOW, target )
+		ParticleManager:SetParticleControl( numberIndex, 1, Vector( 1, mana_to_burn, 0 ) )
+	    ParticleManager:SetParticleControl( numberIndex, 2, Vector( life_time, digits, 0 ) )
+		local burnIndex = ParticleManager:CreateParticle( burn_particle_name, PATTACH_ABSORIGIN, target )
+		
+		-- Create timer to properly destroy particles
+		Timers:CreateTimer( life_time, function()
+				ParticleManager:DestroyParticle( numberIndex, false )
+				ParticleManager:DestroyParticle( burnIndex, false)
+				return nil
+			end
+		)
+	end
+	for itemSlot=0,5 do
+		local item = caster:GetItemInSlot(itemSlot)
+		if item ~= nil then
+			local itemName = item:GetName()
+			if (itemName == "item_mana_thief") then
+				item:StartCooldown(17)
+			end
+		end
+	end
+end
+
+function Shock( keys )
+	-- Variables
+	local caster = keys.caster
+	local target = keys.target
+	local current_mana = target:GetMana()
+	local burn_amount = 50
+	local number_particle_name = "particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn_msg.vpcf"
+	local burn_particle_name = "particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn.vpcf"
+	local damageType = keys.ability:GetAbilityDamageType()
+	if (keys.target.mana_thief == nil) and (caster.nobuorb1 == "item_mana_thief") then
+		keys.target.mana_thief = 1
+		-- Calculation
+		local mana_to_burn = math.min( current_mana,  burn_amount)
+		local life_time = 2.0
+		local digits = string.len( math.floor( mana_to_burn ) ) + 1
+		
+		-- Fail check
+		if target:IsMagicImmune() then
+			mana_to_burn = 0
+		end
+		
+		-- Apply effect of ability
+		target:ReduceMana( mana_to_burn )
+		local damageTable = {
+			victim = target,
+			attacker = caster,
+			damage = mana_to_burn,
+			damage_type = damageType
+		}
+		Timers:CreateTimer(0.1, function() 
+							keys.target.mana_thief = nil
+				end)
+		ApplyDamage( damageTable )
+		
+		-- Show VFX
+		if mana_to_burn ~= 0 then
+			local numberIndex = ParticleManager:CreateParticle( number_particle_name, PATTACH_OVERHEAD_FOLLOW, target )
+			ParticleManager:SetParticleControl( numberIndex, 1, Vector( 1, mana_to_burn, 0 ) )
+		    ParticleManager:SetParticleControl( numberIndex, 2, Vector( life_time, digits, 0 ) )
+			local burnIndex = ParticleManager:CreateParticle( burn_particle_name, PATTACH_ABSORIGIN, target )
+			
+			-- Create timer to properly destroy particles
+			Timers:CreateTimer( life_time, function()
+					ParticleManager:DestroyParticle( numberIndex, false )
+					ParticleManager:DestroyParticle( burnIndex, false)
+					return nil
+				end
+			)
+		end
+	end
+end
+
+function Shock2( keys )
+	-- Variables
+	local caster = keys.caster
+	local target = keys.target
+	local current_mana = target:GetMana()
+	local burn_amount = 80
+	local number_particle_name = "particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn_msg.vpcf"
+	local burn_particle_name = "particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn.vpcf"
+	local damageType = keys.ability:GetAbilityDamageType()
+	if (keys.target.mana_thief == nil) and (caster.nobuorb1 == "item_mana_thief") then
+		keys.target.mana_thief = 1
+		-- Calculation
+		local mana_to_burn = math.min( current_mana,  burn_amount)
+		local life_time = 2.0
+		local digits = string.len( math.floor( mana_to_burn ) ) + 1
+		
+		-- Fail check
+		if target:IsMagicImmune() then
+			mana_to_burn = 0
+		end
+		
+		-- Apply effect of ability
+		Timers:CreateTimer(0.1, function() 
+					keys.target.mana_thief = nil
+				end)
+		target:ReduceMana( mana_to_burn )
+		local damageTable = {
+			victim = target,
+			attacker = caster,
+			damage = mana_to_burn,
+			damage_type = damageType
+		}
+		ApplyDamage( damageTable )
+		
+		-- Show VFX
+		if mana_to_burn ~= 0 then
+			local numberIndex = ParticleManager:CreateParticle( number_particle_name, PATTACH_OVERHEAD_FOLLOW, target )
+			ParticleManager:SetParticleControl( numberIndex, 1, Vector( 1, mana_to_burn, 0 ) )
+		    ParticleManager:SetParticleControl( numberIndex, 2, Vector( life_time, digits, 0 ) )
+			local burnIndex = ParticleManager:CreateParticle( burn_particle_name, PATTACH_ABSORIGIN, target )
+			
+			-- Create timer to properly destroy particles
+			Timers:CreateTimer( life_time, function()
+					ParticleManager:DestroyParticle( numberIndex, false )
+					ParticleManager:DestroyParticle( burnIndex, false)
+					return nil
+				end
+			)
+		end
+	end
+end
