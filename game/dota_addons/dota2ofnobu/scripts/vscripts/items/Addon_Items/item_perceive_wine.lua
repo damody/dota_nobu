e8874b3979550d15f301defd60fe7a3fb3af3329
damody <t1238142000@gmail.com>
2017-01-05 22:12:03 +0800

    fix 說明

--- game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_perceive_wine.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_perceive_wine.lua
@@ -11,23 +11,12 @@ function Shock( keys )
 	end
 	-- Strong Dispel 刪除負面效果
 	caster:Purge( false, true, true, true, true)
-	Timers:CreateTimer(0.1, function() 
-			if (caster:HasModifier("modifier_perceive_wine")) then
-				if caster:HasModifier("Passive_liquor") then
-					caster:RemoveModifierByName("Passive_liquor")
-				end
-				if caster:HasModifier("Passive_sake") then
-					caster:RemoveModifierByName("Passive_sake")
-				end
-				return 1
-			end
-			return nil
-		end)
+	
 	local sumt = 0
 	Timers:CreateTimer(0.1, function()
 		sumt = sumt + 0.1
 		if sumt < Time then
-			if (not caster:HasModifier("modifier_perceive_wine")) then
+			if (not caster:HasModifier("modifier_perceive_wine")) and caster.nomagic == nil then
 				ability:ApplyDataDrivenModifier(caster, caster,"modifier_perceive_wine",{duration=(Time-sumt)})
 			end
 			return 0.1
