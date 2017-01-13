6b7f0fd87035aa5005a86fe166e2b84dc4e6e898
damody <t1238142000@gmail.com>
2017-01-13 19:46:56 +0800

    up a31 c21

--- game/dota_addons/dota2ofnobu/scripts/vscripts/equilibrium_constant.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/equilibrium_constant.lua
@@ -7,15 +7,15 @@
 
 -- 
 local HP_PER_STR = 25
-local HP_REGEN_PER_STR = 0.1
+local HP_REGEN_PER_STR = 0.2
 local MANA_PER_INT = 19
-local MANA_REGEN_PER_INT = 0.1
+local MANA_REGEN_PER_INT = 0.15
 local ARMOR_PER_AGI = 0.333
 local ATKSPD_PER_AGI = 2
 local MAX_MS = 3000
 
 -- default value from dota
-local DEFAULT_HP_PER_STR = 19
+local DEFAULT_HP_PER_STR = 20
 local DEFAULT_HP_REGEN_PER_STR = 0.03
 local DEFAULT_MANA_PER_INT = 13
 local DEFAULT_MANA_REGEN_PER_INT = 0.04
