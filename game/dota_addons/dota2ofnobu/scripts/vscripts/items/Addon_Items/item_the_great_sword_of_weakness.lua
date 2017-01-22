156ac41763e11dba8fff24dff45fa2423d1afd03
damody <t1238142000@gmail.com>
2017-01-22 11:06:47 +0800

    修正部份物品施法前搖問題 說明問題

--- game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_weakness.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_weakness.lua
@@ -6,8 +6,8 @@ function Shock( keys )
 	local target = keys.target
 	local ability = keys.ability
     local time = caster:GetIntellect()/40*0.6+0.6
-    if target:IsMagicImmune() and time > 1 then
-        time = 1
+    if target:IsMagicImmune() and time > 1.5 then
+        time = 1.5
     end
     target:AddNewModifier(caster, ability, "modifier_weakness", {duration = time})
 end
