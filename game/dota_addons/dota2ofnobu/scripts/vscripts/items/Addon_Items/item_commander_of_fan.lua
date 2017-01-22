33ac9acafdb0cd419de1a43592f79e8de215bed4
damody <t1238142000@gmail.com>
2017-01-22 18:34:30 +0800

    南蠻胴具足 統率者鐵扇 天之叢雲

--- game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_commander_of_fan.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_commander_of_fan.lua
@@ -20,6 +20,7 @@ function Shock( keys )
 	end)
 	
 	for _,target in pairs(direUnits) do
+		AMHC:Damage(caster,target, 1,AMHC:DamageType( "DAMAGE_TYPE_PURE" ) )
 		if not target:IsBuilding() then
 			if (target:IsMagicImmune()) then
 				ability:ApplyDataDrivenModifier(caster,target,"modifier_commander_of_fan1",nil)
--- game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_weakness.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_weakness.lua
@@ -9,6 +9,7 @@ function Shock( keys )
     if target:IsMagicImmune() and time > 1.5 then
         time = 1.5
     end
+    AMHC:Damage(caster,keys.target, 1,AMHC:DamageType( "DAMAGE_TYPE_PURE" ) )
     target:AddNewModifier(caster, ability, "modifier_weakness", {duration = time})
 end
 
