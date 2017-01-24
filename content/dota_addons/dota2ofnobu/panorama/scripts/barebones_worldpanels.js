4429010ce0d5e0a00d6277c7861487a0db087770
KXY <KXY@home.com>
2017-01-24 13:25:36 +0800

    把煩人的加號變不見

--- content/dota_addons/dota2ofnobu/panorama/scripts/barebones_worldpanels.js
+++ content/dota_addons/dota2ofnobu/panorama/scripts/barebones_worldpanels.js
@@ -245,6 +245,7 @@ function UpdateEntities()
 { 
 
   // 暫時先讓所有和天賦樹的UI都點不到
+  $.Msg("Disable Talent Tree")
   var x = $.GetContextPanel().GetParent().GetParent().GetParent();
   x = x.FindChildTraverse('HUDElements')
   x = x.FindChildTraverse('lower_hud')
@@ -253,11 +254,16 @@ function UpdateEntities()
 
   x = x.FindChildTraverse('center_with_stats')
   x = x.FindChildTraverse('center_block')
-
-  var lv_stats = x.FindChildTraverse('level_stats_frame')
-  lv_stats.hittest = false
-  lv_stats.hittestchildren = false
-
+  var level_stats_frame = x.FindChildTraverse('level_stats_frame')
+  level_stats_frame.RemoveClass('CanLevelStats')
+  level_stats_frame.hittest = false
+  level_stats_frame.hittestchildren = false
+  level_stats_frame.FindChildTraverse('LevelUpBurstFX').visible = false
+  var LevelUpTab = level_stats_frame.FindChildTraverse('LevelUpTab')
+  var LevelUpButton = LevelUpTab.FindChildTraverse('LevelUpButton')
+  var LevelUpIcon = LevelUpTab.FindChildTraverse('LevelUpIcon')
+  LevelUpButton.visible = false
+  LevelUpIcon.visible = false
   x = x.FindChildTraverse('AbilitiesAndStatBranch')
   x = x.FindChildTraverse('StatBranch')
   x.hittest = false
