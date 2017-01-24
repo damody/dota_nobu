816c4773d0a2cd3654558e69c7521b0d944bbc96
damody <t1238142000@gmail.com>
2017-01-25 04:19:58 +0800

    大典名刀技能沒傷害

--- game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_sakabatou.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_sakabatou.lua
@@ -3,39 +3,41 @@ function Shock( keys )
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
-	local ran =  RandomInt(0, 100)
-	if (caster.sakabatou_count == nil) then
-		caster.sakabatou_count = 0
-	end
-	if (caster.sakabatou == nil) then
-			caster.sakabatou = 1
-				Timers:CreateTimer(0.1, function() 
-					caster.sakabatou = nil
-				end)
-		if (ran > 11) then
-			caster.sakabatou_count = caster.sakabatou_count + 1
-		end
-		if (caster.sakabatou_count > 10 or ran <= 11) then
+	if not caster:IsIllusion() then
+		local ran =  RandomInt(0, 100)
+		if (caster.sakabatou_count == nil) then
 			caster.sakabatou_count = 0
-			StartSoundEvent( "Hero_SkeletonKing.CriticalStrike", keys.target )
-
-			if (not(target:IsBuilding())) then
-				if target:IsMagicImmune() then
-					ability:ApplyDataDrivenModifier(caster,target,"modifier_sakabatou",{duration = 0.2})
-				else
-					ability:ApplyDataDrivenModifier(caster,target,"modifier_sakabatou",{duration = 0.8})
-				end
+		end
+		if (caster.sakabatou == nil) then
+				caster.sakabatou = 1
+					Timers:CreateTimer(0.1, function() 
+						caster.sakabatou = nil
+					end)
+			if (ran > 11) then
+				caster.sakabatou_count = caster.sakabatou_count + 1
 			end
-			
-			--動作
-			local rate = caster:GetAttackSpeed()
-			--print(tostring(rate))
-			--播放動畫
-		    --caster:StartGesture( ACT_SLAM_TRIPMINE_ATTACH )
-			if rate < 1.00 then
-			    caster:StartGestureWithPlaybackRate(ACT_DOTA_ECHO_SLAM,1.00)
-			else
-			    caster:StartGestureWithPlaybackRate(ACT_DOTA_ECHO_SLAM,rate)
+			if (caster.sakabatou_count > 10 or ran <= 11) then
+				caster.sakabatou_count = 0
+				StartSoundEvent( "Hero_SkeletonKing.CriticalStrike", keys.target )
+
+				if (not(target:IsBuilding())) then
+					if target:IsMagicImmune() then
+						ability:ApplyDataDrivenModifier(caster,target,"modifier_sakabatou",{duration = 0.2})
+					else
+						ability:ApplyDataDrivenModifier(caster,target,"modifier_sakabatou",{duration = 0.8})
+					end
+				end
+				
+				--動作
+				local rate = caster:GetAttackSpeed()
+				--print(tostring(rate))
+				--播放動畫
+			    --caster:StartGesture( ACT_SLAM_TRIPMINE_ATTACH )
+				if rate < 1.00 then
+				    caster:StartGestureWithPlaybackRate(ACT_DOTA_ECHO_SLAM,1.00)
+				else
+				    caster:StartGestureWithPlaybackRate(ACT_DOTA_ECHO_SLAM,rate)
+				end
 			end
 		end
 	end
--- game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_banish.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/items/Addon_Items/item_the_great_sword_of_banish.lua
@@ -4,7 +4,7 @@ function Shock( keys )
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
-
+	AMHC:Damage(caster,keys.target, caster:GetIntellect()*3.5,AMHC:DamageType( "DAMAGE_TYPE_MAGICAL" ) )
     ability:ApplyDataDrivenModifier(caster,target,"modifier_great_sword_of_banish",{duration = 3})
     Timers:CreateTimer(0.1, function ()
 		if target:HasModifier("modifier_great_sword_of_banish") then
