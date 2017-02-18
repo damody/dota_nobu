165da47f616ff11e792ba1ffc24b4ff19765a87d
damody <t1238142000@gmail.com>
2017-02-18 21:10:59 +0800

    慶次有圈圈了 提示隊友 對面看不到

--- game/dota_addons/dota2ofnobu/scripts/vscripts/heroes/C_Neutral/C05.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/heroes/C_Neutral/C05.lua
@@ -108,12 +108,12 @@ function C05T_OnSpellStart( keys )
 	local ability = keys.ability
 	
 	-- 提示攻擊範圍
-	--[[
+	
 	local dummy = CreateUnitByName("npc_dummy_unit_new",point,false,nil,nil,caster:GetTeamNumber())
 	dummy:AddNewModifier(dummy,nil,"modifier_kill",{duration=5})
-	local ifx = ParticleManager:CreateParticle("particles/c05/c05t_aoe_hint.vpcf",PATTACH_ABSORIGIN,dummy)
+	local ifx = ParticleManager:CreateParticleForTeam("particles/c05/c05t_aoe_hint.vpcf",PATTACH_ABSORIGIN,dummy, caster:GetTeamNumber())
 	ParticleManager:ReleaseParticleIndex(ifx)
-	]]
+	
 
 	Timers:CreateTimer(ability:GetChannelTime()-0.3, function()
 		if ability:IsChanneling() then
