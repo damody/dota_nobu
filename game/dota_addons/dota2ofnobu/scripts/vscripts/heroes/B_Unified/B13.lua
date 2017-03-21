3e117887c0016f089ddd2ff73ead59339f63b699
Nian <niank430@gmail.com>
2017-03-21 22:20:25 +0800

    新增舊版百地 技能都照字面說明 我認為需要調一下數值 修正縮地 遁地死後復活能使用的bug

--- game/dota_addons/dota2ofnobu/scripts/vscripts/heroes/B_Unified/B13.lua
+++ game/dota_addons/dota2ofnobu/scripts/vscripts/heroes/B_Unified/B13.lua
@@ -19,6 +19,10 @@ function B13D( keys )
 	end
 end
 
+function B13D_remove( keys )
+	keys.caster:FindAbilityByName("B13B"):SetActivated(false)
+end
+
 function B13D_onCreated( keys )
 	keys.ability:SetLevel(1)
 end
@@ -92,7 +96,7 @@ function B13T( keys )
 
 	ability:ApplyDataDrivenModifier( caster, dummy, "modifier_B13T_veryslowAura", nil)
 
-	local radius = 500
+	local radius = ability:GetSpecialValueFor("B13T_radius")
 	local time = 0.1 + 5.5
 	local count = 0
 --
@@ -127,7 +131,7 @@ function B13T( keys )
 	end)
 end
 
-
+---------THIS BLOCK NEVER RUN---------------
 function B13T_OnIntervalThink( keys )
 	local caster = keys.caster
 	local ability = keys.ability
@@ -139,4 +143,161 @@ function B13T_OnIntervalThink( keys )
 			ability:ApplyDataDrivenModifier(caster,enemy,"modifier_B13T_veryslow",{duration = 1})
 		end
 	end
+end
+--------------------------------------------
+--old背刺
+function B13D_old( keys )
+	local attacker = keys.attacker
+	local target = keys.target
+	local ability = keys.ability
+	local targetAngle = VectorToAngles(target:GetForwardVector()) 
+	local attackerAngle = VectorToAngles(attacker:GetForwardVector())
+	local angle = targetAngle[2]-attackerAngle[2]
+
+	if angle < 0 then
+		angle = angle * -1
+	end
+
+	if angle < 90 or angle > 270 then
+		if not target:IsBuilding() then
+			local dmgt = {
+						victim = target,
+						attacker = attacker,
+						ability = ability,
+						damage = ability:GetSpecialValueFor("B13D_old_damage") ,
+						damage_type = ability:GetAbilityDamageType(),
+						damage_flags = DOTA_DAMAGE_FLAG_NONE,
+			}
+			ApplyDamage(dmgt)
+		end
+	end
+end
+--old遁地
+function B13W_old( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	if caster:HasModifier("modifier_B13W_old_underground") == false then
+		ability:ApplyDataDrivenModifier( caster, caster, "modifier_B13W_old_underground", {} )
+		caster:AddNewModifier(caster, ability, "modifier_B13D", nil )
+	else
+		caster:RemoveModifierByName("modifier_B13W_old_underground")
+		caster:RemoveModifierByName("modifier_B13D")
+	end
+end
+--old地雷 ref A26D
+function B13R_old( keys )
+	local caster = keys.caster
+	local center = keys.target_points[1]
+	local ability = keys.ability
+
+
+	local active_delay = 1
+	local mine = CreateUnitByName("A26_MINE", center, false, caster, caster, caster:GetTeamNumber())
+	mine:SetOwner(caster)
+	mine:SetHullRadius(ability:GetSpecialValueFor("B13R_old_radius_detector"))
+	mine:SetBaseMaxHealth( ability:GetSpecialValueFor("B13R_old_hp") )
+	mine:SetHealth( mine:GetMaxHealth() )
+	ability:ApplyDataDrivenModifier(caster,mine,"modifier_B13R_old_rooted",{})
+	Timers:CreateTimer( active_delay, function()
+		if IsValidEntity(mine) and mine:IsAlive() then
+			ability:ApplyDataDrivenModifier( mine, mine,"modifier_B13R_old_detectorAura", nil )
+		end
+	end)
+
+	EmitSoundOn("Hero_Techies.StasisTrap.Plant",caster)
+end
+
+--old地雷爆炸 ref A26D
+function B13R_old_explosion( keys )
+	local caster = keys.caster -- mine
+	local ability = keys.ability
+	local radius_explosion = ability:GetSpecialValueFor("B13R_old_radius_explosion")
+
+	-- 搜尋
+	local units = FindUnitsInRadius(caster:GetTeamNumber(),	-- 關係參考
+		caster:GetAbsOrigin(),			-- 搜尋的中心點
+		nil, 							-- 好像是優化用的參數不懂怎麼用
+		radius_explosion,				-- 搜尋半徑
+		ability:GetAbilityTargetTeam(),	-- 目標隊伍
+		ability:GetAbilityTargetType(),	-- 目標類型
+		ability:GetAbilityTargetFlags(),-- 額外選擇或排除特定目標
+		FIND_ANY_ORDER,					-- 結果的排列方式
+		false) 							-- 好像是優化用的參數不懂怎麼用
+
+	-- 處理搜尋結果
+	local attacker = ability:GetCaster()
+	for _,unit in ipairs(units) do
+		local damageTable = {
+			victim = unit,
+			attacker = caster,
+			ability = ability,
+			damage = ability:GetSpecialValueFor("B13R_old_damage"),
+			damage_type = ability:GetAbilityDamageType(),
+			damage_flags = DOTA_DAMAGE_FLAG_NONE
+		}
+		if unit:IsHero() then
+			damageTable.damage = damageTable.damage * 0.7
+		end
+		ApplyDamage(damageTable)
+	end
+
+	local center = caster:GetAbsOrigin()
+	local ifx = ParticleManager:CreateParticle("particles/units/heroes/hero_techies/techies_land_mine_explode.vpcf",PATTACH_POINT,caster)
+	ParticleManager:SetParticleControl(ifx,0,center)
+	ParticleManager:SetParticleControl(ifx,1,Vector(0,0,radius_explosion))
+	ParticleManager:SetParticleControl(ifx,2,Vector(1,0,0))
+	ParticleManager:ReleaseParticleIndex(ifx)
+
+	EmitSoundOn("Hero_Techies.LandMine.Detonate",caster)
+	caster:RemoveModifierByName("modifier_B13R_old_detectorAura")
+	caster:ForceKill(true)
+end
+
+--T_old 忍法密傳．暴風
+function B13T_old( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local point = ability:GetCursorPosition()
+	local tickPerSec = 10
+
+	local dummy = CreateUnitByName( "npc_dummy_unit", point, false, nil, nil, caster:GetTeamNumber())
+	dummy:AddNewModifier( dummy, nil, "modifier_kill", {duration=7} )
+	dummy:SetOwner(caster)
+	dummy:AddAbility( "majia_vison"):SetLevel(1)
+
+	ability:ApplyDataDrivenModifier( caster, dummy, "modifier_B13T_old_veryslowAura", nil)
+
+	local radius = ability:GetSpecialValueFor("B13T_old_radius")
+	local time = 0.1 + 7
+	local count = 0
+--
+	Timers:CreateTimer(0,function()
+		count = count + 1 / tickPerSec
+		if count > time then
+			return nil
+		end
+		local units = FindUnitsInRadius(caster:GetTeamNumber(), point, nil, radius, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), ability:GetAbilityTargetFlags(), FIND_ANY_ORDER, false )
+		for _,unit in ipairs(units) do
+			if unit:IsBuilding() then
+				ApplyDamage({
+					victim = unit,
+					attacker = caster,
+					ability = ability,
+					damage = ability:GetSpecialValueFor("B13T_old_damage") / tickPerSec * 0.3,
+					damage_type = ability:GetAbilityDamageType(),
+					damage_flags = DOTA_DAMAGE_FLAG_NONE,
+				})
+			else
+				ApplyDamage({
+					victim = unit,
+					attacker = caster,
+					ability = ability,
+					damage = ability:GetSpecialValueFor("B13T_old_damage") / tickPerSec,
+					damage_type = ability:GetAbilityDamageType(),
+					damage_flags = DOTA_DAMAGE_FLAG_NONE,
+				})
+			end
+		end	
+		return 1 / tickPerSec
+	end)
 end
